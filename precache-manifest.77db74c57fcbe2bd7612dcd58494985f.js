9ee38464d0927fd20dc5c5dfd586de3c9100f975
dukad <du.kad@northeastern.edu>
2023-09-10 19:49:13 -0600

    Updates

---
+++ precache-manifest.77db74c57fcbe2bd7612dcd58494985f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a87d2fa2a74fac411b5a",
+    "url": "/static/css/main.742c8ee0.chunk.css"
+  },
+  {
+    "revision": "a87d2fa2a74fac411b5a",
+    "url": "/static/js/main.8787d2b7.chunk.js"
+  },
+  {
+    "revision": "d6fbda626ec98883463b",
+    "url": "/static/js/runtime~main.0deb1fd8.js"
+  },
+  {
+    "revision": "5287ad4c9b6e90e523c9",
+    "url": "/static/js/2.149133ef.chunk.js"
+  },
+  {
+    "revision": "089bc876b02613239fa5",
+    "url": "/static/js/3.b7c4cbb6.chunk.js"
+  },
+  {
+    "revision": "184efd781dfec489fe36861a8e577bb2",
+    "url": "/static/media/bass.184efd78.jpg"
+  },
+  {
+    "revision": "72a187ccec502a15c3482cee0634dd7b",
+    "url": "/static/media/Tanzania.72a187cc.jpg"
+  },
+  {
+    "revision": "6d9d479a936c2e2e9ce8accfc178d943",
+    "url": "/static/media/relax.6d9d479a.jpg"
+  },
+  {
+    "revision": "ac0510f708b3df2462364b2e6518d461",
+    "url": "/static/media/sciencecenter.ac0510f7.jpg"
+  },
+  {
+    "revision": "86f7ce247e4730ea492ca4afdf850a84",
+    "url": "/static/media/Snowshoe.86f7ce24.JPG"
+  },
+  {
+    "revision": "78fe36466d1722d6168806400cc89409",
+    "url": "/index.html"
+  }
+];
